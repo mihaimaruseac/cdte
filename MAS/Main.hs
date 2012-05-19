687f9f2453a812cf954f4942d562767b42114614
Mihai Maruseac <mihai@rosedu.org>
2012-05-19 11:16:30 +0300

    Parse command line arguments.

--- MAS/Main.hs
+++ MAS/Main.hs
@@ -1,3 +1,16 @@
 module MAS.Main where
 
-mainMAS = print "Works"
+import System.Environment (getArgs)
+
+-- Main entry point of the application.
+mainMAS :: IO ()
+mainMAS = do
+  args <- getArgs
+  let fName = getSystemFile args
+  print fName
+
+-- Returns the name of the file containing the description of the system.
+getSystemFile :: [String] -> String
+getSystemFile [] = "system.txt"
+getSystemFile [f] = f
+getSystemFile _ = error "Provide only the filename."
