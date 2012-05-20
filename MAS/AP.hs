70f16373798a0f398d0596b48349ceb5df86cdf1
Mihai Maruseac <mihai@rosedu.org>
2012-05-20 23:49:45 +0300

    Also plan helpings.

--- MAS/AP.hs
+++ MAS/AP.hs
@@ -15,6 +15,8 @@ import Data.List (sortBy, (\\))
 import MAS.GenericTypes
 import MAS.Messages
 
+type IncomingTask = (Task, Maybe Cost, AP)
+
 -- Return True if I have no task to do (at all).
 finished :: AP -> Bool
 finished a@(AP { leftOvers=lo }) = lo == []
@@ -44,7 +46,7 @@ planifyTasks a@(AP {budget=b, caps=c, leftOvers=lo, afap=afap,
     taskInfo <- askAboutOthers afap a all_tasks
     writeChan afap $ DoneAsking aid
     -- find tasks only I can do
-    let myTasks = sortBy (capacity c) $ map fst $ filter (\(_,l) -> l == []) taskInfo
+    let myTasks = sortBy (myCapacity c) $ map fst $ filter (\(_,l) -> l == []) taskInfo
     -- send proposals
     let toProposeTasks = filter (\(_, l) -> l /= []) taskInfo
     forM_ toProposeTasks (proposeTasksToAgents a)
@@ -55,17 +57,28 @@ planifyTasks a@(AP {budget=b, caps=c, leftOvers=lo, afap=afap,
     -- try to do as many of my tasks as possible
     let (newBudget, todoTasks, toLeaveTasks) = planMine myTasks b c
     print ("MM-mine", newBudget, todoTasks, toLeaveTasks, aid)
+    -- then, try to do as many of the helping tasks as possible
+    let (newBudget', todoTasks', toLeaveTasks') = planHelping (sortBy (incomingSort c) helpingTasks) newBudget c
+    print ("MM-help", newBudget', todoTasks', toLeaveTasks', aid)
     -- TODO: discard wrong tasks
     -- try to solve some tasks
     -- TODO distribute tasks to other agents
     return ([], all_tasks)
 
-capacity :: [Cap] -> Task -> Task -> Ordering
-capacity cap (_, c1) (_, c2) =
+myCapacity :: [Cap] -> Task -> Task -> Ordering
+myCapacity cap (_, c1) (_, c2) =
   case lookup c1 cap of
     Nothing -> error "Invalid task list (no one can do task)"
     Just co1 -> case lookup c2 cap of
       Nothing -> error "Invalid task list (no one can do task)"
+      Just co2 -> co1 `compare` co2
+
+incomingSort :: [Cap] -> IncomingTask -> IncomingTask -> Ordering
+incomingSort cap ((_, c1), _, _) ((_, c2), _, _) =
+  case lookup c1 cap of
+    Nothing -> error "This shouldn't happen"
+    Just co1 -> case lookup c2 cap of
+      Nothing -> error "This shouldn't happen"
       Just co2 -> co1 `compare` co2
 
 planMine :: [Task] -> Cost -> [Cap] -> (Cost, [Task], [Task])
@@ -74,6 +87,15 @@ planMine ts b c = doPlan ts b c [] []
     doPlan [] b _ todo toleave = (b, todo, toleave)
     doPlan all@(t@(tid, cid):ts) b c todo toleave = case lookup cid c of
       Nothing -> error "This should not have happened"
+      Just cost -> if cost <= b then doPlan ts (b - cost) c (t:todo) toleave
+        else doPlan [] b c todo all
+
+planHelping :: [IncomingTask] -> Cost -> [Cap] -> (Cost, [IncomingTask], [IncomingTask])
+planHelping ts b c = doPlan ts b c [] []
+  where
+    doPlan [] b _ todo toleave = (b, todo, toleave)
+    doPlan all@(t@((tid, cid), _, _):ts) b c todo toleave = case lookup cid c of
+      Nothing -> error "Impossible situation"
       Just cost -> if cost <= b then doPlan ts (b - cost) c (t:todo) toleave
         else doPlan [] b c todo all
 
